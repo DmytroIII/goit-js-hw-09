8df90d097573f1866879eee05e83eb5c120812a7
Filatov Dmytro <dmitrovfilatov@gmail.com>
2022-10-11 21:42:37 +0300

    1 dz

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,6 +3,19 @@
 
 
 const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+let timerId = null;
 
-console.log(buttonStart);
+buttonStart.addEventListener("click", () => {
+    timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
 
+buttonStop.addEventListener("click", () => { 
+    clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
