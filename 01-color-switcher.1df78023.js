0090f7c4fd2384b42cf8c1c3df6d7e581ecc09c7
DmytroIII <76039937+DmytroIII@users.noreply.github.com>
2022-10-11 17:18:23 +0000

    Deploying to gh-pages from @ DmytroIII/goit-js-hw-09@9d4c8c13ebecf0724d4d3b0b1969114a74f3f31b 🚀

--- 01-color-switcher.1df78023.js
+++
@@ -1,2 +0,0 @@
-console.log("Privet"),console.log("Privests");
-//# sourceMappingURL=01-color-switcher.1df78023.js.map
--- 01-color-switcher.aa5e64c1.js
+++
@@ -1,2 +0,0 @@
-console.log("Privet"),console.log("Privests");
-//# sourceMappingURL=01-color-switcher.aa5e64c1.js.map
